b4926a3375701fb32164d3dc9d15aef38d4c03a5
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2015-02-11 00:49:31 +0100

    Fixed potential infinite loop in ADJUST-VECTOR-EXPONENTIALLY
    ADJUST-VECTOR-EXPONENTIALLY went into an infinite loop when the current
    size of the vector was 0 since double the size make no progress in that
    case.
    
    Signed-off-by: Paul M. Rodriguez <pmr@ruricolist.com>

--- dom/dom-impl.lisp
+++ dom/dom-impl.lisp
@@ -222,9 +222,8 @@
 (defun adjust-vector-exponentially (vector new-dimension set-fill-pointer-p)
   (let ((d (array-dimension vector 0)))
     (when (< d new-dimension)
-      (loop
-	  do (setf d (* 2 d))
-	  while (< d new-dimension))
+      (loop do (setf d (max 1 (* 2 d)))
+         while (< d new-dimension))
       (adjust-array vector d))
     (when set-fill-pointer-p
       (setf (fill-pointer vector) new-dimension))))
