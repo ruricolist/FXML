58ea3385403a0716bc4500a80e3fcfb54cb1cb20
Paul M. Rodriguez <pmr@ruricolist.com>
2015-06-15 19:14:19 -0500

    Targets for transformation.

--- html5/transform.lisp
+++ html5/transform.lisp
@@ -1,13 +1,13 @@
 (in-package #:fxml.html5)
 
 (defmethod html5-parser:transform-html5-dom ((to-type (eql 'xhtml)) node &key)
-  (serialize-dom node (cxml:make-string-sink)))
+  (serialize-dom node (fxml:make-string-sink)))
 
 (defmethod html5-parser:transform-html5-dom ((to-type (eql 'dom)) node &key)
-  (serialize-dom node (cxml-dom:make-dom-builder)))
+  (serialize-dom node (fxml-dom:make-dom-builder)))
 
 (defmethod html5-parser:transform-html5-dom ((to-type (eql 'stp)) node &key)
-  (serialize-dom node (cxml-stp:make-builder)))
+  (serialize-dom node (fxml.stp:make-builder)))
 
 (defmethod html5-parser:transform-html5-dom ((to-type (eql 'xmls)) node &key)
-  (serialize-dom node (cxml-xmls:make-xmls-builder)))
+  (serialize-dom node (fxml.xmls:make-xmls-builder)))
