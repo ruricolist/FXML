a803bd03297b23a73c25bc4a0a72df71a50af096
Paul M. Rodriguez <pmr@ruricolist.com>
2015-06-24 16:39:16 -0500

    Methods for check-insertion-allowed, replace-child.

--- stp/document-fragment.lisp
+++ stp/document-fragment.lisp
@@ -12,6 +12,17 @@
       (append-child result (copy child)))
     result))
 
+(defmethod check-insertion-allowed ((parent document-fragment) child i)
+  (declare (ignore i))
+  (check-type child node)
+  (assert-orphan child)
+  (typecase child
+    (element
+     (when (childp parent child)
+       (stp-error "attempt to add a node as its own descendant")))
+    ((or comment processing-instruction text))
+    (t (stp-error "not a valid child of an element: ~A" child))))
+
 (defmethod insert-child ((parent parent-node) (fragment fxml.stp:document-fragment) pos)
   (let* ((children (node.children fragment))
          (len (length children)))
@@ -21,5 +32,16 @@
             do (setf (node.parent child) parent)
                (insert-child parent child pos)))
     parent))
+
+(defmethod replace-child ((parent parent-node) old-child (fragment fxml.stp:document-fragment))
+  (declare (optimize (debug 0)))
+  (labels ((rec ()
+             (case (length (node.children fragment))
+               (0)
+               (1 (replace-child parent old-child (detach (first-child fragment))))
+               (t (let ((new-child (detach (last-child fragment))))
+                    (insert-child-after parent old-child new-child)
+                    (rec))))))
+    (rec)))
 
 
