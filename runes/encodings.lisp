6be7914d3c82b60eda3413d55253f9d876d8ee3d
Paul M. Rodriguez <pmr@ruricolist.com>
2016-01-22 15:36:25 -0600

    Revert "Revert "Drop special handling for UTF-16.""
    This reverts commit 43e0ea43a11a07ac12620775597ae0a5191b41d2.

--- runes/encodings.lisp
+++ runes/encodings.lisp
@@ -2,8 +2,7 @@
 
 (eval-when (:compile-toplevel :load-toplevel)
   (alexandria:define-constant +buffer-byte+
-    #+rune-is-utf-16 '(unsigned-byte 16)
-    #-rune-is-utf-16 '(unsigned-byte 32)
+      '(unsigned-byte 32)
     :test 'equal))
 
 (define-condition encoding-error (simple-error) ())
@@ -186,7 +185,6 @@
 		 (y (logior (ash hi2 8) lo2)))
 	    (unless (<= #xDC00 x #xDFFF)
 	      (xerror "expected a high surrogate but found: #x~X" x))
-	    #-rune-is-utf-16
 	    (progn
 	      (setf x (logior (ash (%- x #xd7c0) 10) (%and y #x3FF)))
 	      (setf rptr (%+ 2 rptr))))
@@ -223,7 +221,6 @@
 		 (y (logior (ash hi2 8) lo2)))
 	    (unless (<= #xDC00 x #xDFFF)
 	      (xerror "expected a high surrogate but found: #x~X" x))
-	    #-rune-is-utf-16
 	    (progn
 	      (setf x (logior (ash (%- x #xd7c0) 10) (%and y #x3FF)))
 	      (setf rptr (%+ 2 rptr))))
@@ -253,7 +250,6 @@
 				(eql x #xFFFE)
 				(eql x #xFFFF))
                             (xerror "not a valid code point: #x~X" x))
-			   #+rune-is-utf-16
 		           ((%> x #xFFFF)
                             (setf (aref out (%+ 0 wptr)) (%+ #xD7C0 (ash x -10))
                                   (aref out (%+ 1 wptr)) (%ior #xDC00 (%and x #x3FF)))
