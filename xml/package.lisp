b927ceee07667d3b384af6e20c6c21cfc7a193df
Paul M. Rodriguez <pmr@ruricolist.com>
2017-08-07 16:43:51 -0500

    Import serapeum:defstruct-read-only.

--- xml/package.lisp
+++ xml/package.lisp
@@ -9,7 +9,7 @@
   (:use :cl :fxml.runes :fxml.runes-encoding :split-sequence)
   (:import-from #:named-readtables #:in-readtable)
   (:import-from #:alexandria #:compose #:curry #:rcurry)
-  (:import-from #:serapeum #:with-thunk)
+  (:import-from #:serapeum #:with-thunk #:defstruct-read-only)
   (:export
    ;; meta
    #:xml-character-p
