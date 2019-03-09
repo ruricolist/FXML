679e71af7eb5031cc1f948619737dfb398ee197c
Paul M. Rodriguez <pmr@ruricolist.com>
2019-03-08 21:30:19 -0600

    Add fxml.html as a nickname for fxml.html5.

--- html5/package.lisp
+++ html5/package.lisp
@@ -2,6 +2,7 @@
 
 (defpackage #:fxml.html5
   (:use #:cl #:alexandria #:serapeum)
+  (:nicknames #:fxml.html)
   (:export #:serialize-dom #:make-html5-sink #:close-sink
            #:xhtml #:dom #:stp #:xmls
            #:parse))
